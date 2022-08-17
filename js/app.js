54927d3079cf9e695b277253a7dd1f4b4b15b9a8
Olalekan Oluwaseyi Bolade <lekanseyibolade@gmail.com>
2022-08-18 00:43:35 +0100

    Completed mobile view

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const openBtn = document.querySelector(".open-menu-btn");
+const closeBtn = document.querySelector(".close-menu-btn");
+const nav = document.querySelector("nav");
+
+openBtn.addEventListener('click' , () => {
+    nav.classList.add('show-nav');
+})
+
+closeBtn.addEventListener('click' , () => {
+    nav.classList.remove('show-nav');
+})
